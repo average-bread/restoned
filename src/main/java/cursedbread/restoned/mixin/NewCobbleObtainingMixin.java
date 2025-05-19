fa3db99c0c0a2eee5537e7f0107b81acc787c1e6
average-bread <163648686+average-bread@users.noreply.github.com>
2025-05-19 14:06:00 +0200

    cobbled marble and slate fix

--- src/main/java/cursedbread/restoned/mixin/NewCobbleObtainingMixin.java
+++ src/main/java/cursedbread/restoned/mixin/NewCobbleObtainingMixin.java
@@ -13,7 +13,10 @@ import net.minecraft.core.item.IItemConvertible;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.Items;
 import net.minecraft.core.world.World;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,10 +27,13 @@ import java.util.Objects;
 @Mixin(value = BlockLogic.class, remap = false)
 
 public class NewCobbleObtainingMixin {
+	@Shadow
+	@Final
+	@NotNull
+	public Block<?> block;
+
 	@Inject(method = "getBreakResult(Lnet/minecraft/core/world/World;Lnet/minecraft/core/enums/EnumDropCause;IIIILnet/minecraft/core/block/entity/TileEntity;)[Lnet/minecraft/core/item/ItemStack;", at=@At("TAIL"), cancellable = true)
 	public void getBreakResult(World world, EnumDropCause dropCause, int x, int y, int z, int meta, TileEntity tileEntity, CallbackInfoReturnable<ItemStack[]> cir) {
-
-		Block block = world.getBlock(x, y, z);
 
 		if (block == Blocks.MARBLE){
 			switch (dropCause) {
@@ -36,9 +42,11 @@ public class NewCobbleObtainingMixin {
 				case EXPLOSION:
 				case PROPER_TOOL:
 					cir.setReturnValue(new ItemStack[]{new ItemStack((IItemConvertible) RestonedBlocks.COBBLE_MARBLE)});
+					return;
 				case PICK_BLOCK:
 				case SILK_TOUCH:
 					cir.setReturnValue(new ItemStack[]{new ItemStack((IItemConvertible) Blocks.MARBLE)});
+					return;
 				default:
 					cir.setReturnValue(null);
 			}
@@ -51,9 +59,11 @@ public class NewCobbleObtainingMixin {
 				case EXPLOSION:
 				case PROPER_TOOL:
 					cir.setReturnValue(new ItemStack[]{new ItemStack((IItemConvertible) RestonedBlocks.COBBLE_SLATE)});
+					return;
 				case PICK_BLOCK:
 				case SILK_TOUCH:
 					cir.setReturnValue(new ItemStack[]{new ItemStack((IItemConvertible) Blocks.SLATE)});
+					return;
 				default:
 					cir.setReturnValue(null);
 			}
